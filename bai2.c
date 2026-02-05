dbb04eba794ea7b3591bd1afae64cfab51a93279
longtoc1108-prog <crynotsadpvp@gmail.com>
2026-02-04 21:12:06 -0700

    Văn Huy nộp bài

--- bai2.c
+++ bai2.c
@@ -9,30 +9,5 @@
 //  Output: Xuất ra màn hình giá trị nhỏ nhất và lớn nhất của mảng
 
 // VIẾT CODE Ở ĐÂY
-#include <stdio.h>
 
-int main() {
-    int n;
-    printf("Nhập số phần tử của mảng: ");
-    scanf("%d", &n);
-    int arr[n];
-    printf("Nhập các phần tử của mảng:\n");
-    for (int i = 0; i < n; i++) {
-        scanf("%d", &arr[i]);
-    }
-    int max = arr[0];
-    int min = arr[0];
-    for (int i = 1; i < n; i++) {
-        if (arr[i] > max) {
-            max = arr[i];
-        }
-        if (arr[i] < min) {
-            min = arr[i];
-        }
-    }
-    printf("Giá trị lớn nhất trong mảng: %d\n", max);
-    printf("Giá trị nhỏ nhất trong mảng: %d\n", min);
-
-    return 0;
-}
 
