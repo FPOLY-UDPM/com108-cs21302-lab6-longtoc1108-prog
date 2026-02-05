a299f9cf11fc2b80b3cd0e7e34f24ef21207dc1e
longtoc1108-prog <crynotsadpvp@gmail.com>
2026-02-04 21:14:29 -0700

    Văn Huy nộp bài

--- bai1.c
+++ bai1.c
@@ -14,27 +14,31 @@
 
 int main() {
     int n, sum = 0, count = 0;
-    float average;
+
     printf("Nhập số phần tử của mảng: ");
     scanf("%d", &n);
+
     int arr[n];
+
     printf("Nhập các phần tử của mảng:\n");
     for (int i = 0; i < n; i++) {
         scanf("%d", &arr[i]);
     }
+
     for (int i = 0; i < n; i++) {
         if (arr[i] % 3 == 0) {
             sum += arr[i];
             count++;
         }
     }
+
     if (count > 0) {
-        average = (float)sum / count;
-        printf("Trung bình tổng các số chia hết cho 3 trong mảng: %.2f\n", average);
+        printf("Trung bình tổng các số chia hết cho 3 trong mảng: %.2f\n", (float)sum / count);
     } else {
         printf("Không có số nào chia hết cho 3 trong mảng.\n");
     }
 
     return 0;
 }
+
 
